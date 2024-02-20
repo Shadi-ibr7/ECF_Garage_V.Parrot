fede3e07fa1e51f489dc49fa764100cc55fe939d
Shadi-ibr7 <150540163+Shadi-ibr7@users.noreply.github.com>
2024-02-20 21:30:41 +0100

    Add files via upload

---
+++ ECF_Garage V. Parrot/login system/index.php
@@ -0,0 +1,17 @@
+<?php
+
+@include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="member_area.php">Afficher tous les membres</a>
+</body>
+</html>
---
+++ ECF_Garage V. Parrot/sql/sql.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `espace_admin`.`membre` ( `id` INT NOT NULL AUTO_INCREMENT , `pseudo` TEXTE NOT NULL , `email` TEXTE NOT NULL , `mot_de_passe` TEXTE NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
