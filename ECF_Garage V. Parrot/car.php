249158d1376bebca8e414719c0f6292cf7c0de4a
Shadi-ibr7 <150540163+Shadi-ibr7@users.noreply.github.com>
2024-02-24 18:04:04 +0100

    Create car.php

---
+++ ECF_Garage V. Parrot/car.php
@@ -0,0 +1,105 @@
+<section class="vehicles" id="vehicles">
+
+    <h1 class="heading"><span>vehicles populaire</span> </h1>
+
+    <div class="swiper vehicles-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-1.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 156000km
+                    </p>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-2.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 160000km
+                    </p>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-3.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 100000km
+                    </p>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-4.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 110000km
+                    </p>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-5.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 120000km
+                    </p>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="image/vehicle-6.png" alt="">
+                <div class="content">
+                    <h3>new model</h3>
+                    <div class="price"> <span>price : </span> €62,000/- </div>
+                    <p>
+                        new
+                        <span class="fas fa-circle"></span> 2021
+                        <span class="fas fa-circle"></span> automatique
+                        <span class="fas fa-circle"></span> essence
+                        <span class="fas fa-circle"></span> 90000km
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="swiper-pagination"></div>
+
+    </div>
+
+</section>
