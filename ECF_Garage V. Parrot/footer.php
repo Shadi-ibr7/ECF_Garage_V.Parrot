6b6a5131e356fb3ad00bbcf98ff98e31ab05000d
Shadi-ibr7 <150540163+Shadi-ibr7@users.noreply.github.com>
2024-02-24 18:03:37 +0100

    Create footer.php

---
+++ ECF_Garage V. Parrot/footer.php
@@ -0,0 +1,45 @@
+<footer class="footer" id="footer">
+
+    <section class="box-container">
+
+        <div class="box">
+            <h3>horaire d'ouvertur</h3>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Lun. : 08h30-12h00, 14h00 - 18h30 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Mar. : 08h30-12h00, 14h00 - 18h30 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Mer. : 08h30-12h00, 14h00 - 18h30 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Jeu. : 08h30-12h00, 14h00 - 18h30 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Ven. : 08h30-12h00, 14h00 - 18h30 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Sam. : 08h30-12h00 </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Lun. : Fermé </a>
+        </div>
+
+        <div class="box">
+            <h3>Liens rapides</h3>
+            <a href="#home"> <i class="fas fa-arrow-right"></i> Accueil</a>
+            <a href="#vehicles"> <i class="fas fa-arrow-right"></i> Véhicles</a>
+            <a href="#services"> <i class="fas fa-arrow-right"></i> Services</a>
+            <a href="#featured"> <i class="fas fa-arrow-right"></i> Vedette</a>
+            <a href="#reviews"> <i class="fas fa-arrow-right"></i> Avis</a>
+            <a href="#contact"> <i class="fas fa-arrow-right"></i> contact</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Toulous, France </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+        </div>
+
+    </section>
+
+    <div class="credit"> <p>&copy; 2023 Chadi Ibrahim | Tous droits réservés</p> </div>
+
+</footer>
